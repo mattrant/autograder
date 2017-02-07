9037932cc206f4312c0688a4f4abdfe6380f2199
mattrant <mattrant@buffalo.edu>
2017-02-07 07:46:53 -0500

    Adds compilation checking and score initialization
    Student's score is initialized to a list containing the same
    amount of zeros as there are possible inputs.

--- script_tester/official_autograder_master.py
+++ script_tester/official_autograder_master.py
@@ -1,7 +1,7 @@
 import json
 from copy import deepcopy
-from subprocess import call,Popen,PIPE
-
+from subprocess import call,Popen,PIPE,check_output,CalledProcessError
+from sys import stdout,exit
 class autograder_outline:
     #file containing the anwser key
     grading_key = None
@@ -10,9 +10,11 @@ class autograder_outline:
     def __init__(self):
         #encode as json to ensure dissambiguation of return value (ie 6 vs "6")
         correct_output = open("grading_key.txt","r")
-        self.grading_key = json.loads(correct_output.read())
+        file_contents = correct_output.read()
+        print file_contents
+        self.grading_key = json.loads(file_contents)
         self.student_response = deepcopy(self.grading_key)
-        self.clear_student_grades()
+        self.clear_student_info()
         self.grade()
 
     def clear_student_info(self):
@@ -24,26 +26,62 @@ class autograder_outline:
         #'expected output' key with 'your output'
         for problem in self.student_response:
             for test_case in self.student_response[problem]:
-                self.student_response[problem][test_case]["score"] = []
                 del self.student_response[problem][test_case]["expected output"]
-                self.student_response[problem][test_case]["your output"] = []
+                scores = len(self.grading_key[problem][test_case]["input"])
+                self.student_response[problem][test_case]["score"] = [0 for score in range(scores)]
 
     def grade(self):
         """
         Initiates the grading process for every file present in the grading_key
         dictionary
         """
-        for problem in a.grading_key:
-            print problem,":"
+        for problem in self.grading_key:
+
             file_output_name= problem.split(".")[0]
 
-            if not can_compile(problem) :
+            if not self.can_compile(problem) :
+                # for test_case in self.grading_key[problem]:
+                #     for score in self.grading_key[problem][test_case]["score"]:
+                #         self.student_response[problem][test_case]["score"].append(0)
                 continue
 
-            for test_case in a.grading_key[problem]:
+            for test_case in self.grading_key[problem]:
+                i = 0
+                for program_input in self.grading_key[problem][test_case]["input"]:
+
+                #     p = Popen('./'+file_output_name, stdin = PIPE, stdout = PIPE)
+                #     p.wait()
+                #     result = p.stdout.read()
+                # if result == self.grading_key[problem][test_case]["expected"][i]:
+                #     self.student_response[problem][test_case]["score"][i]= 20
+                # else:
+                #     self.student_response[problem][test_case]["score"][i] = 0
+                    pass
+
+        self.report_grade()
+
+    def report_grade(self):
+        """
+        Creates JSON output for autograder from scores in student_response
+        """
+        self.print_dictionary(self.student_response)
+        self.print_dictionary(self.grading_key)
+
+    def print_dictionary(self,dictionary):
+        """
+        Prints out the homework related dictionaries in a nice human readable
+        format
+        Input:
+            dictionary: the dictionary to be printed
+        """
+        print "*"*40
+        for problem in dictionary:
+            print problem,":"
+            for test_case in dictionary[problem]:
                 print test_case,":"
-                for entry in a.grading_key[problem][test_case]:
-                    print "\t",entry,":",a.grading_key[problem][test_case][entry]
+                for entry in dictionary[problem][test_case]:
+                    print "\t",entry,":",dictionary[problem][test_case][entry]
+        print "*"*40
 
     def can_compile(self,file_name):
         """
@@ -53,23 +91,15 @@ class autograder_outline:
             file_name: the name of the file to be compiled
         """
         file_output_name= file_name.split(".")[0]
+        cmd_string = "gcc "+file_name+" -o "+file_output_name
         try:
-            call(['gcc', file_name, '-o', file_output_name])
+            check_output(cmd_string.split(" "),)
             return True
-        #TODO: replace this error with the error that will get thrown
-        except Error as e:
-            print "Error has occurred"
+        except CalledProcessError as e:
+            print "Exiting grading for",file_name,"..."
             return False
-            #TODO:set all grades for this problem to 0 if it failes
+        except OSError as e:
+            print "Exiting grading for",file_name,"..."
+            return False
 
 a = autograder_outline()
-print "~"*40
-
-for problem in a.grading_key:
-    print problem,":"
-    for test_case in a.grading_key[problem]:
-        print test_case,":"
-        for entry in a.grading_key[problem][test_case]:
-            print "\t",entry,":",a.grading_key[problem][test_case][entry]
-        #if type of test_case is list ....
-    print "*"*40
